52feb62187f04d5f53d1ae4fda5788693db2ede6
Hritika Sanjay Dhasal <66423487+HellFire03@users.noreply.github.com>
2021-05-09 23:54:06 +0530

    Update code.py

--- Easy/Shortest word distance/code.py
+++ Easy/Shortest word distance/code.py
@@ -3,11 +3,11 @@ words = ["Practice", "makes", "perfect", "coding", "makes"]
 #words = list(input().split(" "))
 word1 = input()
 word2 = input()
-minimun = sys.maxsize
+minimum = sys.maxsize
 for i in range(len(words)):
     if words[i] == word1:
         for j in range(len(words)):
             if words[j] == word2:
-                if (abs(i-j)<minimun):
-                    minimun = abs(i-j)
-print(minimun)
+                if (abs(i-j)<minimum):
+                    minimum = abs(i-j)
+print(minimum)
